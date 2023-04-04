8dccc66617913452e05f1207789a9eba201e5698
ghimiresunil <sunilghimire64@gmail.com>
2023-04-04 14:13:15 +0545

    Update: main file to run helper file

--- src/helper.py
+++ src/helper.py
@@ -141,34 +141,5 @@ class FuseBot:
             if "goodbye" in user_input.lower() or "bye" in user_input.lower():
                 break
         
-if __name__ == '__main__':
-    bot = FuseBot()
-    query_response_count = 0
-    hard_skill_response_count = 0
-    soft_skill_response_count = 0
-    hr_message_count = 0
-    goodbye_response_count = 0
-    
-    while query_response_count < 2:  # loop until two query responses
-        bot.query_response()
-        query_response_count += 1
-    
-    while hard_skill_response_count < 4:  # loop until four hard skill responses
-        bot.hard_skill_response()
-        hard_skill_response_count += 1
-        print('*'*50)
-        print(hard_skill_response_count)
-        print('*'*50)
 
-    while soft_skill_response_count < 2:
-        bot.soft_skill_response()
-        soft_skill_response_count += 1
-    
-    while hr_message_count < 3:
-        bot.hr_message_reponse('index.json')
-        hr_message_count += 1
-    
-    while goodbye_response_count < 1:
-        bot.good_bye_response()
-        goodbye_response_count += 1
     
--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
 from helper import FuseBot
 
-if __name__ == "__main__":
-    fb = FuseBot()
-    hard_skills=['pandas','docker','github']
-    while(True):
-        inp = input("Enter User Text: ")
-        res = fb.get_answer_in_text(inp,hard_skills)
-        print("RES:", res)
+if __name__ == '__main__':
+    bot = FuseBot()
+    query_response_count = 0
+    hard_skill_response_count = 0
+    soft_skill_response_count = 0
+    hr_message_count = 0
+    goodbye_response_count = 0
+    
+    while query_response_count < 2:  # loop until two query responses
+        bot.query_response()
+        query_response_count += 1
+    
+    while hard_skill_response_count < 4:  # loop until four hard skill responses
+        bot.hard_skill_response()
+        hard_skill_response_count += 1
+        print('*'*50)
+        print(hard_skill_response_count)
+        print('*'*50)
+
+    while soft_skill_response_count < 2:
+        bot.soft_skill_response()
+        soft_skill_response_count += 1
+    
+    while hr_message_count < 3:
+        bot.hr_message_reponse('index.json')
+        hr_message_count += 1
+    
+    while goodbye_response_count < 1:
+        bot.good_bye_response()
+        goodbye_response_count += 1
