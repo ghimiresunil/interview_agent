63cb5c68ea023aa583efff4bbb56aa82de08fc28
ghimiresunil <sunilghimire64@gmail.com>
2023-04-04 13:15:39 +0545

    Update: Stream response for welcome response, hard skills, soft skills, and hr response

--- src/helper.py
+++ src/helper.py
@@ -149,24 +149,24 @@ if __name__ == '__main__':
     hr_message_count = 0
     goodbye_response_count = 0
     
-    # while query_response_count < 2:  # loop until two query responses
-    #     bot.query_response()
-    #     query_response_count += 1
+    while query_response_count < 2:  # loop until two query responses
+        bot.query_response()
+        query_response_count += 1
     
-    # while hard_skill_response_count < 4:  # loop until four hard skill responses
-    #     bot.hard_skill_response()
-    #     hard_skill_response_count += 1
-    #     print('*'*50)
-    #     print(hard_skill_response_count)
-    #     print('*'*50)
+    while hard_skill_response_count < 4:  # loop until four hard skill responses
+        bot.hard_skill_response()
+        hard_skill_response_count += 1
+        print('*'*50)
+        print(hard_skill_response_count)
+        print('*'*50)
 
-    # while soft_skill_response_count < 2:
-    #     bot.soft_skill_response()
-    #     soft_skill_response_count += 1
+    while soft_skill_response_count < 2:
+        bot.soft_skill_response()
+        soft_skill_response_count += 1
     
-    # while hr_message_count < 3:
-    #     bot.hr_message_reponse('index.json')
-    #     hr_message_count += 1
+    while hr_message_count < 3:
+        bot.hr_message_reponse('index.json')
+        hr_message_count += 1
     
     while goodbye_response_count < 1:
         bot.good_bye_response()
